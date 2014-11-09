9c5b074de72e560a2f2ef876b223ddb989bcc9f4
Anna <anna@safronovs.com>
2014-11-08 22:04:20 -0500

    svg turned into html + animation started

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Close Your Tags!</title>
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/main.css">
+	<link rel="stylesheet" href="css/svg.css">
+	<link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+	<script src="js/game.js"></script>
+</head>
+<body>
+
+    <header id="start-page" class="contain">
+        <h1>Close your tags</h1>
+        
+    </header>
+
+    <section id="game-field" class="contain">
+    	<p class="tile">&lt;p&gt;</p>
+    	<p class="tile">&lt;div&gt;</p>
+    	<p class="tile">&lt;script&gt;</p>
+    	<p class="tile">&lt;head&gt;</p>
+    	<p class="tile">&lt;p&gt;</p>
+		
+		<p class="tile">&lt;p&gt;</p>
+		<p class="tile">&lt;p&gt;</p>
+		<p class="tile">&lt;/p&gt;</p>
+		<p class="tile">&lt;/script&gt;</p>
+		<p class="tile">&lt;/div&gt;</p>
+
+		<p class="tile">&lt;p&gt;</p>
+		<p class="tile">&lt;/body&gt;</p>
+		<p class="tile">&lt;p&gt;</p>
+		<p class="tile">&lt;p&gt;</p>
+		<p class="tile">&lt;/a&gt;</p>
+
+		<p class="tile">&lt;p&gt;</p>
+		<p class="tile">&lt;p&gt;</p>
+		<p class="tile">&lt;/head&gt;</p>
+		<p class="tile">&lt;p&gt;</p>
+		<p class="tile">&lt;body&gt;</p>	
+    </section>
+
+	<?php include "svg/composition.php"; ?>
+
+	
+</body>
+</html>
